6361a31fb4b9833d8d6f2d5ed9c020046246891e
CursedIndividual <mehceu@gmail.com>
2022-02-15 21:07:22 +0300

    array was covered, just getting started

---
+++ 01-array/array.js
@@ -0,0 +1,23 @@
+var nums = new Array();
+
+for (var i = 0; i < 20; i++) {
+    nums.push(i);
+}
+
+console.log(nums);
+
+
+
+// creating a matrix
+var matrix = [];
+for (var j = 0; j < 5; j++) {
+    matrix[i] = new Array();
+}
+
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+        matrix.push(i * j);
+    }
+}
+
+console.log(matrix);
