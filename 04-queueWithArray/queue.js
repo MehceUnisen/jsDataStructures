d37050056c145a2df1bd7bb4335a13e6ba2e14e7
CursedIndividual <mehceu@gmail.com>
2022-02-15 23:17:38 +0300

    implemented queue with stacks :art:

---
+++ 04-queueWithArray/queue.js
@@ -0,0 +1,39 @@
+class Queue {
+
+    queue = [];
+    queueLength = 1;
+    lastQueuedElement = 0;
+
+    constructor(firstValue) {
+        this.firstValue = firstValue;
+        this.queue[0] = firstValue;
+    }
+
+    queueElement(element) {
+        this.lastQueuedElement = element;
+        this.queue[this.queueLength] = element;
+        ++this.queueLength;
+    }
+
+    dequeueElement() {
+        if (this.queueLength === 1 && this.queue[0] == this.lastQueuedElement) {
+            console.log("Deleting the queue!");
+            this.deleteQueue();
+        } else if (this.queueLength === 1 && this.queue[0] != this.lastQueuedElement) {
+            console.log("Queue is already empty!");
+        } else {
+            this.queue.splice(0, 1);
+            --this.queueLength;
+        }
+    }
+
+    printQueue() {
+        console.log(this.queue);
+    }
+
+    deleteQueue() {
+        this.queue = [];
+        this.queueLength = 1;
+        return;
+    }
+}
