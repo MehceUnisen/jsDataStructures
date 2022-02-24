3723cd070ab5dc306e01246754553e5a50a6fc58
CursedIndividual <mehceu@gmail.com>
2022-02-24 15:11:08 +0300

    doesnt work!!

--- 13-binarySearchTree/binarySearchTree.js
+++ 13-binarySearchTree/binarySearchTree.js
@@ -20,36 +20,52 @@ class BinaryTree {
             this.iter.left = new Node(data);
             this.iter.left.left = null;
             this.iter.left.right = null;
-        }
-        if (direction == 1) {
+        } else if (direction == 1) {
             this.iter.right = new Node(data);
             this.iter.right.left = null;
             this.iter.right.right = null;
+        } else if (direction == 2) {
+            if (this.iter === this.root.right) {
+                console.log("neden ki acep");
+            }
+            this.root.right = new Node(data);
+            // this.iter.right = null;
+            // this.iter.left = null;
         }
     }
 
     setDirection(data) {
         this.iter = this.root;
-        while (this.iter.left != null || this.iter.right != null) {
-            if (this.iter.left === null && this.iter.right.data > data) {
-                return 0; //left
-            } else if (this.iter.right === null && this.iter.left.data <= data) {
-                return 1; //right
-            } else if (this.iter.data >= data) {
-                this.iter = this.iter.left;
+        while (this.iter != null) {
+
+            if (this.iter.left == null && this.iter.right == null) {
+                if (this.iter.left === null && this.iter.right != null) {
+                    if (this.iter.right.data > data) {
+                        return 0; //left
+                    }
+                } else if (this.iter.right === null && this.iter.left != null) {
+                    if (this.iter.left.data > data) {
+                        return 1; //right
+                    }
+                } else if (this.iter.data >= data) {
+                    console.log(this.iter.data);
+                    this.iter = this.iter.left;
+                } else {
+                    console.log(this.iter.data);
+                    this.iter = this.iter.right;
+                }
             } else {
-                this.iter = this.iter.right;
+                return 0;
             }
         }
 
-        return 0;
+        return 2;
     }
 
 }
 
-var bst = new BinaryTree(10);
-bst.addNode(9);
-bst.addNode(8);
+var bst = new BinaryTree(7);
 bst.addNode(11);
+// bst.addNode(9);
 
-console.log(bst.root)
+console.log(bst.root);
